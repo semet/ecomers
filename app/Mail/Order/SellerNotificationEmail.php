9c81edd66c1dc0ace20cf0e1859d87da59a0df60
Semet <hamdanilombok@gmail.com>
2022-04-22 00:02:24 +0800

    add Donation

---
+++ app/Mail/Order/SellerNotificationEmail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail\Order;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SellerNotificationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $order;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.order.seller-notification');
+    }
+}
