f2eb7dd0e77cf11c118d043976d65ef7f442354c
Semet <hamdanilombok@gmail.com>
2022-04-26 22:38:46 +0800

    add admin layout

---
+++ app/Http/Livewire/Member/Account/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Member\Account;
+
+use Livewire\Component;
+
+class Login extends Component
+{
+    public function render()
+    {
+        return view('livewire.member.account.login');
+    }
+}
